6dfe1ece18839f2de0e5477af50a033847214311
Otto Allmendinger <mail@ttll.de>
2019-10-27 14:59:15 +0100

    Daemon: implement main loop
    Add support for graceful shutdown

--- cmd/daemon/main.go
+++ cmd/daemon/main.go
@@ -4,6 +4,8 @@ import (
 	"github.com/0xb10c/bademeister-go/src/daemon"
 	"log"
 	"flag"
+	"os"
+	"os/signal"
 )
 
 var zmqHost = flag.String("zmq-host", "127.0.0.1", "zmq host")
@@ -15,8 +17,26 @@ func main() {
 
 	log.Println("Starting Bademeister Daemon")
 
-	_, err := daemon.NewBademeisterDaemon(*zmqHost, *zmqPort, *dbPath)
+	d, err := daemon.NewBademeisterDaemon(*zmqHost, *zmqPort, *dbPath)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt)
+		s := <- c
+		log.Printf("Received signal %s, shutting down", s)
+		d.Stop()
+	}()
+
+	if err := d.Run(); err != nil {
+		log.Printf("Error %v, shutting down", err)
+	}
+
+	if err := d.Close(); err != nil {
+		log.Fatalln("Error during shutdown")
+	}
+
+	os.Exit(0)
 }
--- src/daemon/daemon.go
+++ src/daemon/daemon.go
@@ -3,12 +3,15 @@ package daemon
 import (
 	"fmt"
 	"github.com/0xb10c/bademeister-go/src/storage"
+	"github.com/0xb10c/bademeister-go/src/types"
 	"github.com/0xb10c/bademeister-go/src/zmqsubscriber"
+	"log"
 )
 
 type BademeisterDaemon struct {
 	zmqSub     	*zmqsubscriber.ZMQSubscriber
 	storage 	*storage.Storage
+	quit 		chan struct{}
 }
 
 // NewBademeisterDaemon initiates a new BademeisterDaemon.
@@ -22,5 +25,63 @@ func NewBademeisterDaemon(host, port, dbPath string) (*BademeisterDaemon, error)
 	if err != nil {
 		return nil, fmt.Errorf("could not initialize storage: %s", err)
 	}
-	return &BademeisterDaemon{zmqSub, store}, nil
+
+	quit := make(chan struct{}, 1)
+	return &BademeisterDaemon{zmqSub, store, quit}, nil
+}
+
+func (b *BademeisterDaemon) processTransaction(tx *types.Transaction) error {
+	log.Printf("Received transaction, adding to storage")
+	return b.storage.AddTransaction(tx)
+}
+
+func (b *BademeisterDaemon) processBlock(block *types.Block) error {
+	log.Printf("Received block, updating transactions")
+	// TODO update storage
+	return nil
+}
+
+func (b *BademeisterDaemon) Run() error {
+	for {
+		select {
+		case <-b.quit:
+			log.Printf("Received quit signal")
+			return nil
+		case tx := <- b.zmqSub.IncomingTx:
+			if err := b.processTransaction(&tx); err != nil {
+				log.Printf("Error in processTransaction()")
+				return err
+			}
+		case block := <- b.zmqSub.IncomingBlocks:
+			if err := b.processBlock(&block); err != nil {
+				log.Printf("Error in processBlock()")
+				return err
+			}
+		}
+	}
+}
+
+func (b *BademeisterDaemon) Stop() {
+	b.quit <- struct{}{}
+}
+
+func (b *BademeisterDaemon) Close() error {
+	errors := false
+	errZmq := b.zmqSub.Quit()
+	if errZmq != nil {
+		log.Printf("error stopping zmq: %v", errZmq)
+		errors = true
+	}
+
+	errStorage := b.storage.Close()
+	if errStorage != nil {
+		log.Printf("error closing db: %v", errStorage)
+		errors = true
+	}
+
+	if errors {
+		return fmt.Errorf("there were errors, see logs for details")
+	}
+
+	return nil
 }
