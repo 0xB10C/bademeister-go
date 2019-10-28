6b511ed1bc6c6b32b2856d3c4964c08583688ecc
Otto Allmendinger <mail@ttll.de>
2019-10-28 10:32:23 +0100

    Make ZMQSubscriber more robust
    * Move loop into separate `Run() error` func
    * Remove `panic()` from ZMQSubscriber loop
    * Don't close Socket from separate goroutine to avoid triggering crash
      #11

--- src/zmqsubscriber/zmqsubscriber_test.go
+++ src/zmqsubscriber/zmqsubscriber_test.go
@@ -4,44 +4,75 @@ import (
 	"github.com/0xb10c/bademeister-go/src/test"
 	"github.com/0xb10c/bademeister-go/src/types"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
-func TestZMQSubscriber(t *testing.T) {
-	env := test.NewTestEnv()
-	defer env.Quit()
-
-	z, err := NewZMQSubscriber(env.ZmqHost, env.ZmqPort)
-	if err != nil {
-		t.Error(err)
-	}
-
-	defer func() {
-		err := z.Quit()
-		if err != nil {
-			panic(err)
-		}
-	}()
-
+func captureTxs(z *ZMQSubscriber) *[]types.Transaction {
 	txs := []types.Transaction{}
 	go func() {
 		for t := range z.IncomingTx {
 			txs = append(txs, t)
 		}
 	}()
+	return &txs
+}
 
+func captureBlocks(z *ZMQSubscriber) *[]types.Block {
 	blocks := []types.Block{}
 	go func() {
 		for b := range z.IncomingBlocks {
 			blocks = append(blocks, b)
 		}
 	}()
+	return &blocks
+}
+
+func getTestZMQSubscriber(t *testing.T, env test.TestEnv) *ZMQSubscriber {
+	z, err := NewZMQSubscriber(env.ZmqHost, env.ZmqPort)
+	require.NoError(t, err)
+
+	go func() {
+		err := z.Run()
+		require.NoError(t, err)
+	}()
+
+	return z
+}
+
+func TestZMQSubscriber(t *testing.T) {
+	env := test.NewTestEnv()
+	defer env.Quit()
+
+	z := getTestZMQSubscriber(t, env)
+	defer z.Stop()
+
+	txs := captureTxs(z)
+	blocks := captureBlocks(z)
 
 	env.GenerateBlocks(1)
-	assert.Equal(t, 1, len(txs))
-	assert.Equal(t, 1, len(blocks))
+	assert.Equal(t, 1, len(*txs))
+	assert.Equal(t, 1, len(*blocks))
 
 	env.GenerateBlocks(1)
-	assert.Equal(t, 2, len(txs))
-	assert.Equal(t, 2, len(blocks))
+	assert.Equal(t, 2, len(*txs))
+	assert.Equal(t, 2, len(*blocks))
+
+	// Test second subscriber.
+	// This demonstrates that it should be possible to connect multiples instance of
+	// Bademeisterd and have continuous capture.
+	{
+		z2 := getTestZMQSubscriber(t, env)
+		defer z2.Stop()
+
+		txs2 := captureTxs(z2)
+		blocks2 := captureBlocks(z2)
+
+		env.GenerateBlocks(1)
+
+		assert.Equal(t, 3, len(*txs))
+		assert.Equal(t, 3, len(*blocks))
+		assert.Equal(t, 1, len(*txs2))
+		assert.Equal(t, 1, len(*blocks2))
+	}
 }
