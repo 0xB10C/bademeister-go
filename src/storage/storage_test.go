0xB10C/bademeister-go

commit ef71fdd118482ca9936781e01d910876749fc392

switch to INT timestamps in the database
Saving the `firstSeen` transaction timestamps as INTs in Unixtime
reduces space required significantly compared to saving them as TEXT.
https://github.com/0xB10C/bademeister-go/issues/6#issuecomment-547173130

The database version is set to 2.

src/storage/storage.go
@@ -3,13 +3,14 @@ package storage
 import (
 	"database/sql"
 	"fmt"
-	"github.com/0xb10c/bademeister-go/src/types"
-	_ "github.com/mattn/go-sqlite3"
 	"os"
 	"time"
+
+	"github.com/0xb10c/bademeister-go/src/types"
+	_ "github.com/mattn/go-sqlite3"
 )
 
-const VERSION = 1
+const VERSION = 2
 
 type Storage struct {
 	db *sql.DB
@@ -50,12 +51,12 @@ func NewStorage(path string) (*Storage, error) {
 
 func (s *Storage) init(version int) error {
 	sqlStmts := []string{
-		`create table config (version int);`,
-		`create table transactions (
-			txid blob unique,
-			first_seen date,
-			confirmed_block_height integer
-		);`,
+		`CREATE TABLE config (version int)`,
+		`CREATE TABLE transactions (
+			txid BLOB UNIQUE NULL,
+			first_seen INT,
+			confirmed_block_height INT,
+		)`,
 	}
 	for _, stmt := range sqlStmts {
 		if _, err := s.db.Exec(stmt); err != nil {
@@ -64,13 +65,13 @@ func (s *Storage) init(version int) error {
 	}
 
 	_, err := s.db.Exec(
-		`insert into config (version) values (?);`, version,
-	);
+		`INSERT INTO config (version) VALUES (?);`, version,
+	)
 	return err
 }
 
 func (s *Storage) getVersion() (version int) {
-	row := s.db.QueryRow(`select version from config`);
+	row := s.db.QueryRow(`SELECT version FROM config`)
 	if row == nil {
 		panic(fmt.Errorf("could not query version"))
 	}
@@ -103,19 +104,17 @@ func (s *Storage) migrate(fromVersion int) error {
 }
 
 func (s *Storage) AddTransaction(tx *types.Transaction) error {
-	if tx.FirstSeen.Location() != time.UTC {
-		return fmt.Errorf("time must be UTC")
-	}
-
 	// We cannot expect FirstSeen to be monotonic since we might add a tx data
 	// from multiple sources (`getrawmempool`).
 	// https://www.sqlite.org/lang_UPSERT.html
 	_, err := s.db.Exec(`
-		insert into transactions (txid, first_seen) values(?, ?)
-			on conflict(txid) do update set 
-			first_seen = excluded.first_seen
-			where first_seen > excluded.first_seen
-	`, tx.TxID[:], tx.FirstSeen)
+		INSERT INTO transactions (txid, first_seen) VALUES(?, ?)
+		ON CONFLICT(txid) DO
+			UPDATE SET
+				first_seen = excluded.first_seen
+			WHERE
+				first_seen > excluded.first_seen
+	`, tx.TxID[:], tx.FirstSeen.UTC().Unix())
 	return err
 }
 
@@ -139,7 +138,7 @@ func (i *TxIterator) Next() *types.Transaction {
 	var txid types.Hash32
 	copy(txid[:], txidBytes)
 	return &types.Transaction{
-		TxID: txid,
+		TxID:      txid,
 		FirstSeen: firstSeen.UTC(),
 	}
 }
@@ -152,7 +151,7 @@ func (s *Storage) QueryTransactions(q Query) (*TxIterator, error) {
 	var rows *sql.Rows
 	var err error
 
-	baseQuery := `select txid, first_seen from transactions`;
+	baseQuery := `SELECT txid, first_seen FROM transactions`
 
 	if q.FirstSeen == nil {
 		rows, err = s.db.Query(baseQuery)

commit f9188e98d8c451145594046fb76c0b6940041836

fix unit test regression introduced in #15

src/storage/storage.go
@@ -55,7 +55,7 @@ func (s *Storage) init(version int) error {
 		`CREATE TABLE transactions (
 			txid BLOB UNIQUE NULL,
 			first_seen INT,
-			confirmed_block_height INT,
+			confirmed_block_height INT
 		)`,
 	}
 	for _, stmt := range sqlStmts {
@@ -131,7 +131,7 @@ func (i *TxIterator) Next() *types.Transaction {
 		return nil
 	}
 	var txidBytes []byte
-	var firstSeen time.Time
+	var firstSeen int64
 	if err := i.rows.Scan(&txidBytes, &firstSeen); err != nil {
 		panic(err)
 	}
@@ -139,7 +139,7 @@ func (i *TxIterator) Next() *types.Transaction {
 	copy(txid[:], txidBytes)
 	return &types.Transaction{
 		TxID:      txid,
-		FirstSeen: firstSeen.UTC(),
+		FirstSeen: time.Unix(firstSeen, 0).UTC(),
 	}
 }
 
@@ -158,7 +158,7 @@ func (s *Storage) QueryTransactions(q Query) (*TxIterator, error) {
 	} else {
 		rows, err = s.db.Query(
 			fmt.Sprintf("%s where first_seen > ?", baseQuery),
-			q.FirstSeen,
+			q.FirstSeen.Unix(),
 		)
 	}
 

src/storage/storage_test.go
@@ -2,13 +2,14 @@ package storage
 
 import (
 	"fmt"
+	"os"
+	"testing"
+	"time"
+
 	"github.com/0xb10c/bademeister-go/src/test"
 	"github.com/0xb10c/bademeister-go/src/types"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"os"
-	"testing"
-	"time"
 )
 
 func testQueryTransactions(t *testing.T, st *Storage, firstSeen time.Time, txs []types.Transaction) {
@@ -55,8 +56,12 @@ func TestStorage(t *testing.T) {
 	// create from empty file
 	st, err := NewStorage(path)
 	require.NoError(t, err)
-	tm := time.Now().UTC()
 
+	// The nanoseconds are truncated, because the precision is lost
+	// when writing the firstSeen unix timestamp to database.
+	// Not truncating would result in unequal transactions when
+	// comparing with assert.Equal().
+	tm := time.Now().UTC().Truncate(time.Second)
 	txs := []types.Transaction{
 		{
 			TxID:      test.NewTestTxId(nil),